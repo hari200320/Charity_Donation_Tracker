d45de84772a9a9fe666a983a0d84347c3464b320
Hari <hariharanr200318@gmail.com>
2023-10-30 11:57:52 +0530

    Almost done on 11:57am

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { Sidebar, Navbar } from './components';
-
 import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
 
 const App = () => {
@@ -19,11 +18,9 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-campaign" element={<CreateCampaign />} />
-          <Route path="/campaign-details/:id" element={<CampaignDetails />}/>
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
         </Routes>
-
       </div>
-
     </div>
   )
 }
